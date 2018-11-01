b2f3bd88f0d7aee902f92511f6d8f028295f3c47
Flavio Spedaletti <flavio.hfs@gmail.com>
2018-11-01 16:52:06 -0300

    Add files via upload

---
+++ Terrapar.DTO/fechamento_pedreira.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Terrapar.DTO
+{
+    public class fechamento_pedreira
+    {
+        public int id_fechamento_pedreira { get; set; }
+        public decimal valor_refeicao { get; set; }
+        public decimal salario_pedreira { get; set; }
+        public decimal valor_vale { get; set; }
+        public decimal inss_pedreira { get; set; }
+        public int id_usuario { get; set; }
+        public DateTime data { get; set; }
+    }
+}
