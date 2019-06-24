9a87c29b920f63c1777c068ecb307be1d3140dd7
Flavio Spedaletti <flavio.hfs@gmail.com>
2019-06-24 11:19:12 -0300

    - resolvendo cache

--- Backup/Terrapar.DTO/viagem_pao_acucar_destacada.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Terrapar.DTO
-{
-    public class viagem_pao_acucar_destacada
-    {
-        public int id_viagem_pao_acucar_destacada { get; set; }
-        public int id_viagem_pao_acucar { get; set; }
-        public int id_usuario { get; set; }
-    }
-}
--- Terrapar.DTO/viagem_pedreira_destacada.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Terrapar.DTO
-{
-    public class viagem_pedreira_destacada
-    {
-        public int id_viagem_pedreira_destacada { get; set; }
-        public int id_viagem_pedreira { get; set; }
-        public int id_usuario { get; set; }
-    }
-}
